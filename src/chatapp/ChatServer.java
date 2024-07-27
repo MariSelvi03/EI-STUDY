07f5f31654fed1362a26a20bdb35ed50acbe56b7
MariSelvi03 <mariselviramasubramanian@gmail.com>
2024-07-27 23:12:37 +0530

    Update ChatServer.java

--- src/chatapp/ChatServer.java
+++ src/chatapp/ChatServer.java
@@ -1,6 +1,9 @@
 package chatapp;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
@@ -8,38 +11,69 @@ import java.util.Map;
 
 public class ChatServer {
     private static final int PORT = 12345;
-    private static ChatServer instance;
-    private Map<String, ChatRoom> chatRooms;
+    private static Map<String, PrintWriter> clientWriters = new HashMap<>();
 
-    private ChatServer() {
-        chatRooms = new HashMap<>();
-    }
-
-    public static synchronized ChatServer getInstance() {
-        if (instance == null) {
-            instance = new ChatServer();
-        }
-        return instance;
-    }
-
-    public ChatRoom getChatRoom(String roomId) {
-        return chatRooms.computeIfAbsent(roomId, ChatRoom::new);
-    }
-
-    public void start() {
+    public static void main(String[] args) {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.println("Chat server started on port " + PORT);
             while (true) {
                 Socket clientSocket = serverSocket.accept();
-                new ClientHandler(this, clientSocket).start();
+                new ClientHandler(clientSocket).start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
-        ChatServer.getInstance().start();
+    private static class ClientHandler extends Thread {
+        private Socket socket;
+        private PrintWriter out;
+        private BufferedReader in;
+        private String username;
+
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                out = new PrintWriter(socket.getOutputStream(), true);
+
+                // Get username and add to the map
+                username = in.readLine();
+                synchronized (clientWriters) {
+                    clientWriters.put(username, out);
+                }
+
+                String message;
+                while ((message = in.readLine()) != null) {
+                    if (message.equals("DISCONNECT")) {
+                        break;
+                    }
+                    broadcastMessage(username + ": " + message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                synchronized (clientWriters) {
+                    clientWriters.remove(username);
+                }
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        private void broadcastMessage(String message) {
+            synchronized (clientWriters) {
+                for (PrintWriter writer : clientWriters.values()) {
+                    writer.println(message);
+                }
+            }
+        }
     }
 }
-
